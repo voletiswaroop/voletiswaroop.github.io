7a31d0651b2867abb2dbd533be10dd633c4b1fe2
Swaroop Gupta Voleti <vswaroop@athenahealth.com>
2020-09-06 17:04:04 +0530

    added 3d view for awards section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import './App.css'
 import Header from './components/header/header'
 import Announcements from './components/announcements/announcements'
 import AboutMe from './components/about/about'
 import SkillSet from './components/skillset/skills'
 import Experience from './components/experience/experience'
-import Projects from './components/projects/projects'
-import Recognitions from './components/recognitions/recognitions'
 import Education from './components/education/education'
 import Footer from './components/footer/footer'
+
+const Projects = React.lazy(() => import('./components/projects/projects'));
+const Recognitions = React.lazy(() => import('./components/recognitions/recognitions'));
 
 export default class App extends Component {
   animateSections = () => {
@@ -32,8 +33,10 @@ export default class App extends Component {
         <AboutMe />
         <SkillSet />
         <Experience />
-        <Projects />
-        <Recognitions />
+        <Suspense fallback={<div>Loading components...</div>}>
+          <Projects />
+          <Recognitions />
+        </Suspense>
         <Education />
         <Footer />
       </main>
--- src/components/recognitions/recognitions.js
+++ src/components/recognitions/recognitions.js
@@ -2,24 +2,36 @@ import React, { Component } from 'react'
 import './recognitions.css'
 
 export default class recognitions extends Component {
+
+  toggleRubixsAnimation(e) {
+    e.currentTarget.closest('.recoginitions-wrapper').classList.toggle("rubixs-view");
+    e.currentTarget.closest('.recoginitions-wrapper').classList.toggle("default-view");
+  }
   render() {
     return (
-      <section className="recoginitions-wrapper" id="recognitions">
+      <section className="recoginitions-wrapper rubixs-view" id="recognitions">
+        <div className="toggle-view-btns" onClick={(e) => this.toggleRubixsAnimation(e)}>
+          <span>3D</span>
+          <span>2D</span>
+        </div>
         <div className="container">
           <h3><i className="fas fa-award"></i>Awards &amp; Recognitions</h3>
-          <ul className="recoginitions-list">
-            <li><i className="fa fa-circle"></i>Athenahealth
+          <div className="cube-wrapper">
+            <ul className="recoginitions-list">
+              <li className="front">
+                <p><i className="fa fa-circle"></i>Athenahealth</p>
                 <div><i className="fa fa-ellipsis-h" aria-hidden="true"></i>Received Extra mile award twice</div>
-              <div><i className="fa fa-ellipsis-h" aria-hidden="true"></i>Hackathon runner up</div>
-            </li>
-            <li><i className="fa fa-circle"></i>Bankbazaar.com
-             <div><i className="fa fa-ellipsis-h" aria-hidden="true"></i>Received Appreciation award of the year</div>
-              <div><i className="fa fa-ellipsis-h" aria-hidden="true"></i>Hackathon winner </div>
-            </li>
-            <li><i className="fa fa-circle"></i>Innoswift Solutions
-             <div><i className="fa fa-ellipsis-h" aria-hidden="true"></i>Received extra increment as recognition award for the best performance</div>
-            </li>
-          </ul>
+                <div><i className="fa fa-ellipsis-h" aria-hidden="true"></i>Hackathon runner up</div>
+              </li>
+              <li className="left"><p><i className="fa fa-circle"></i>Bankbazaar.com</p>
+                <div><i className="fa fa-ellipsis-h" aria-hidden="true"></i>Received award of appreciation the year</div>
+                <div><i className="fa fa-ellipsis-h" aria-hidden="true"></i>Hackathon winner </div>
+              </li>
+              <li className="right"><p><i className="fa fa-circle"></i>Innoswift Solutions</p>
+                <div><i className="fa fa-ellipsis-h" aria-hidden="true"></i>Received extra increment as recognition award for the best performance</div>
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
     )
